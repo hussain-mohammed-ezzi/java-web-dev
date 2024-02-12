940cbd2d417a06d16990e761eee795b2f779323b
hussain mohammed ezzi <109825958+hussain-mohammed-ezzi@users.noreply.github.com>
2024-02-13 04:24:44 +0500

    Update and rename java-server-creation.txt to java-server-creation.java

---
+++ java-server-creation.java
@@ -0,0 +1,20 @@
+import java.net*;
+import java.io*;
+import javax.swing*;
+public class ServerReadNetEx{
+ public static void main(String args[]){
+   try{
+     // create a server socket \
+     ServerSocket ss = new ServerSocket(2222);
+     System.out.println('Server Started')
+     /// loop back to the accept method nad wait for the clikent to file a connection request , so server will continously listen for the reauest\
+     while(true){
+       Socket s = ss.accept();
+       System.out.Println('conncetion request recived');
+       /// Get IO stream 
+       InputStream is = s.getInputStream()
+         
+     }  
+   }
+ } 
+}
